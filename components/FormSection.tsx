c3733ed5d501a0542651bee9b6ef448ac9f5caf0
nryzhikh <nikita.ryzhikh@outlook.com>
2026-04-21 11:06:52 +0200

    consent

--- components/FormSection.tsx
+++ components/FormSection.tsx
@@ -36,7 +36,7 @@ export function FormSection() {
       phone: fd.get("phone"),
       about: fd.get("about"),
       consentData: fd.get("consentData") === "on",
-      consentRules: fd.get("consentRules") === "on",
+      // consentRules: fd.get("consentRules") === "on",
     };
 
     setSubmitError(null);
